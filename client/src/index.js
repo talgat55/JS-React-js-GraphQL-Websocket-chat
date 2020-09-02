e9e92a3e09489ea6da09af5500669f75babca77b
talgat55 <lightx_design@yahoo.com>
2020-09-03 03:58:14 +0600

    devide client and server also for server add apollo gql  for client add apollo client

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {ApolloClient, ApolloProvider, InMemoryCache} from "@apollo/client";
+
+const client = new ApolloClient({
+    uri: 'http://localhost:4000/',
+    cache: new InMemoryCache()
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ApolloProvider client={client}>
+        <App/>
+    </ApolloProvider>,
   document.getElementById('root')
 );
 
